0f2ac036a40359c95e0ada21c32a22bd43253f8e
Thomas Singer <thomas.singer@syntevo.com>
2016-10-11 10:26:07 +0200

    SG-8500: OS X: "Widget is disposed" while processing keyDown event (bug 143)
    workaround for the SWT error

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
@@ -2450,6 +2450,9 @@ void keyDown (long id, long sel, long theEvent) {
 		Shell s = this.getShell();
 		s.keyInputHappened = false;
 		boolean textInput = OS.objc_msgSend (id, OS.sel_conformsToProtocol_, OS.objc_getProtocol ("NSTextInput")) != 0;
+		if (isDisposed()) {
+			return;
+		}
 		if (!textInput) {
 			// Not a text field, so send a key event here.
 			NSEvent nsEvent = new NSEvent (theEvent);
@@ -2461,6 +2464,9 @@ void keyDown (long id, long sel, long theEvent) {
 		} else {
 			// Control is some kind of text field, so the key event will be sent from insertText: or doCommandBySelector:
 			super.keyDown (id, sel, theEvent);
+			if (isDisposed()) {
+				return;
+			}
 
 			if (imeInComposition ()) return;
 			// If none of those methods triggered a key event send one now.
