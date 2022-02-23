6c9ebbb955764bfad652aba13f0c16ba50766b28
Thomas Singer <ts-swt@syntevo.com>
2022-02-23 14:31:58 +0100

    Bug 578929 - Control.setFocus brings windows to front
    Change-Id: I8c7644e81e67995fb5259eb1cecf5c6862a33307

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
@@ -2912,7 +2912,7 @@ public boolean forceFocus () {
 	if (display.focusEvent == SWT.FocusOut) return false;
 	Shell shell = getShell ();
 	shell.setSavedFocus (this);
-	if (!isEnabled () || !isVisible ()) return false;
+	if (!isEnabled () || !isVisible () || display.getActiveShell() != getShell()) return false;
 	shell.bringToTop (false);
 	return forceFocus (focusHandle ());
 }
