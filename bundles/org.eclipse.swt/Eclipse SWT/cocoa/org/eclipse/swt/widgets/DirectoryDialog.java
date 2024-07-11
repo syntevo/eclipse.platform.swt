2a4a8c3b56ae756bf8f688b68692eb820c5c30b0
Thomas Singer <ts-swt@syntevo.com>
2024-07-11 10:26:01 +0200

    [File|Directory]Dialog.open (MacOS): fix null

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/DirectoryDialog.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/DirectoryDialog.java
@@ -195,7 +195,8 @@ public Optional<String> openDialog () {
 	directoryPath = null;
 	panel = NSOpenPanel.openPanel();
 	if (panel == null) {
-		throw new SWTException(SWT.ERROR_INVALID_RETURN_VALUE);
+//		throw new SWTException(SWT.ERROR_INVALID_RETURN_VALUE);
+		return Optional.empty();
 	}
 
 	callback_performKeyEquivalent = new Callback(this, "_performKeyEquivalent", 3);
