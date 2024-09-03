6c7c08507649b471b09ed6c724f43544caa24acd
Thomas Singer <ts-swt@syntevo.com>
2024-09-03 11:32:04 +0200

    #414: slow StyledTextRenderer for unicode and unprintable control characters
    This test replaces the characters 0x00-0x1F with some alternative
    character (by default '?') for rendering. This improves the performance
    significantly.

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/TextLayout.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/TextLayout.java
@@ -2832,6 +2832,13 @@ public boolean isDisposed () {
 	return device == null;
 }
 
+private static char replaceBelowSpaceCharacters(char chr) {
+	return chr >= ' '
+		? chr
+		//: (char)(0x2400 + chr);
+		: '?';
+}
+
 /*
  *  Itemize the receiver text
  */
@@ -2859,6 +2866,9 @@ StyleItem[] itemize () {
 	int[] pcItems = new int[1];
 	char[] chars = new char[length];
 	segmentsText.getChars(0, length, chars, 0);
+	for (int i = 0; i < length; i++) {
+		chars[i] = replaceBelowSpaceCharacters(chars[i]);
+	}
 	// enable font ligatures
 	scriptControl.fMergeNeutralItems = true;
 	/*
@@ -3735,6 +3745,9 @@ void shape (final long hdc, final StyleItem run) {
 	final int[] buffer = new int[1];
 	final char[] chars = new char[run.length];
 	segmentsText.getChars(run.start, run.start + run.length, chars, 0);
+	for (int i = 0; i < chars.length; i++) {
+		chars[i] = replaceBelowSpaceCharacters(chars[i]);
+	}
 
 	final int maxGlyphs = (chars.length * 3 / 2) + 16;
 	long hHeap = OS.GetProcessHeap();
