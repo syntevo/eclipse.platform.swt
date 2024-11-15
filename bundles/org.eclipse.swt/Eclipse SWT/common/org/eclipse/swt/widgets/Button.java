ba179f8f1228e02321f8c20267fded4ae6759c7d
Heiko Klare <heiko.klare@vector.com>
2024-11-15 19:53:55 +0100

    Consistently move #hasBorder() method up to Control

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Button.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Button.java
@@ -807,11 +807,6 @@ public class Button extends Control implements ICustomWidget {
 		return computedSize;
 	}
 
-	private boolean hasBorder() {
-
-		return (style & SWT.BORDER) != 0;
-	}
-
 	@Override
 	public Point getSize() {
 		return new Point(width, height);
