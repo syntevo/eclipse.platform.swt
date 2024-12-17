89b1cd7401fb243f305045bcf80fdd333bee0dec
Thomas Singer <ts-swt@syntevo.com>
2024-12-17 11:04:04 +0100

    Control (Win32): remove dependency on Browser

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Control.java
@@ -21,7 +21,7 @@ import java.util.stream.*;
 
 import org.eclipse.swt.*;
 import org.eclipse.swt.accessibility.*;
-import org.eclipse.swt.browser.*;
+//import org.eclipse.swt.browser.*;
 import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.internal.*;
@@ -3746,10 +3746,10 @@ private boolean embedsWin32Control () {
 	if (this.isDisposed() || !this.isVisible()) {
 		return false;
 	}
-	if (this instanceof Browser browser) {
-		// The Edge browser embeds webView2
-		return "edge".equals(browser.getBrowserType());
-	}
+//	if (this instanceof Browser browser) {
+//		// The Edge browser embeds webView2
+//		return "edge".equals(browser.getBrowserType());
+//	}
 
 	if (this instanceof OleClientSite) {
 		// OLE objects are always embedded by windows
